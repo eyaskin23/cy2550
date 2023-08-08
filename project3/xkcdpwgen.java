1012a127bc068a72d742e6269b361a5c1b0bf86d
eyaskin23 <evelynyaskin@gmail.com>
2023-08-08 17:30:53 +0000

    project 3

--- project3/xkcdpwgen.java
+++ project3/xkcdpwgen.java
@@ -1,93 +1,18 @@
-import java.security.SecureRandom;
-
-public class xkcdpwgen {
-    static final String[] Words = {
-            "apple", "back", "carry", "numb", "here", "one",
-            "delete", "two", "random", "cookie", "scrabble", "coffee",
-            "mascara", "into", "mono"
-
-    };
-
-    private static final String[] Symbols = {
-            "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[",
-            "]", "{", "}", "|", ":", ";", ",", ".", "<", ">", "/", "?", "~", "`"
-    };
-
-    private static final String[] Numbers = {
-            "1", "2", "3", "4", "5", "6", "7", "8" , "9"
-    };
-
-    public static void main(String[] args) {
-        int numWords = 4;
-        int numCaps = 4;
-        int numNums = 4;
-        int numSymbols = 4;
-
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-h") || args[i].equals("--help")) {
-                printHelp();
-                return;
-            } else if (args[i].equals("-w") || args[i].equals("--words")) {
-                i++;
-                numWords = Integer.parseInt(args[i]);
-            } else if (args[i].equals("-c") || args[i].equals("--caps")) {
-                i++;
-                numCaps = Integer.parseInt(args[i]);
-            } else if (args[i].equals("-n") || args[i].equals("--numbers")) {
-                i++;
-                numNums = Integer.parseInt(args[i]);
-            } else if (args[i].equals("-s") || args[i].equals("--symbols")) {
-                i++;
-                numSymbols = Integer.parseInt(args[i]);
-            }
-        }
-        generatePassword(numWords, numCaps, numNums, numSymbols);
-    }
-
-    private static void printHelp() {
-        System.out.println("Usage: xkcdpwgen [-h] [-w WORDS] [-c CAPS] [-n NUMBERS] [-s SYMBOLS]");
-        System.out.println();
-        System.out.println("Generate a secure, memorable password using the XKCD method");
-        System.out.println();
-        System.out.println("optional arguments:");
-        System.out.println("  -h, --help            show this help message and exit");
-        System.out.println("  -w WORDS, --words WORDS");
-        System.out.println("                        include WORDS words in the password (default=4)");
-        System.out.println("  -c CAPS, --caps CAPS  capitalize the first letter of CAPS random words (default=0)");
-        System.out.println("  -n NUMBERS, --numbers NUMBERS");
-        System.out.println("                        insert NUMBERS random numbers in the password (default=0)");
-        System.out.println("  -s SYMBOLS, --symbols SYMBOLS");
-        System.out.println("                        insert SYMBOLS random symbols in the password (default=0)");
-    }
-
-    private static void generatePassword(int numWords, int numCaps, int numNums, int numSymbols) {
-        SecureRandom random = new SecureRandom();
-        StringBuilder password = new StringBuilder();
-
-        for (int i = 0; i < numWords; i++) {
-            int index = random.nextInt(Words.length);
-            String word = Words[index];
-
-            // Randomly decide whether to capitalize the word
-            if (random.nextBoolean() && i < numCaps) {
-                word = word.substring(0, 1).toUpperCase() + word.substring(1);
-            }
-
-            password.append(word);
-        }
-
-        for (int i = 0; i < numNums; i++) {
-            int index = random.nextInt(Numbers.length);
-            String num = Numbers[index];
-            if (i < numNums) {
-                num = num.substring(0, 1).toUpperCase() + num.substring(1);
-            }
-            password.append(num);
-        }
-        for (int i = 0; i < numSymbols; i++) {
-            int index = random.nextInt(Symbols.length);
-            password.append(Symbols[index]);
-        }
-        System.out.println("Generated password:" + password.toString());
-    }
-}
+[0m[1;31m                          ./+o+-      [0m[1;31m evelynyaskin[0m[1m@[0m[0m[1;31mubuntunm1[0m
+[0m[1;37m                  yyyyy- [0m[1;31m-yyyyyy+     [0m[1;31m OS:[0m Ubuntu 22.04 jammy[0m
+[0m[1;37m               [0m[1;37m://+//////[0m[1;31m-yyyyyyo     [0m[1;31m Kernel:[0m aarch64 Linux 5.15.0-78-generic[0m
+[0m[1;33m           .++ [0m[1;37m.:/++++++/-[0m[1;31m.+sss/`     [0m[1;31m Uptime:[0m 29m[0m
+[0m[1;33m         .:++o:  [0m[1;37m/++++++++/:--:/-     [0m[1;31m Packages:[0m 1781[0m
+[0m[1;33m        o:+o+:++.[0m[1;37m`..```.-/oo+++++/    [0m[1;31m Shell:[0m bash 5.1.16[0m
+[0m[1;33m       .:+o:+o/.[0m[1;37m          `+sssoo+/   [0m[1;31m Resolution:[0m 1440x862[0m
+[0m[1;37m  .++/+:[0m[1;33m+oo+o:`[0m[1;37m             /sssooo.  [0m[1;31m DE:[0m GNOME 41.7[0m
+[0m[1;37m /+++//+:[0m[1;33m`oo+o[0m[1;37m               /::--:.  [0m[1;31m WM:[0m Mutter[0m
+[0m[1;37m \+/+o+++[0m[1;33m`o++o[0m[1;31m               ++////.  [0m[1;31m WM Theme:[0m Adwaita[0m
+[0m[1;37m  .++.o+[0m[1;33m++oo+:`[0m[1;31m             /dddhhh.  [0m[1;31m GTK Theme:[0m Yaru [GTK2/3][0m
+[0m[1;33m       .+.o+oo:.[0m[1;31m          `oddhhhh+   [0m[1;31m Icon Theme:[0m Yaru[0m
+[0m[1;33m        \+.++o+o`[0m[1;31m`-````.:ohdhhhhh+    [0m[1;31m Font:[0m Ubuntu 11[0m
+[0m[1;33m         `:o+++ [0m[1;31m`ohhhhhhhhyo++os:     [0m[1;31m Disk:[0m 14G / 33G (44%)[0m
+[0m[1;33m           .o:[0m[1;31m`.syhhhhhhh/[0m[1;33m.oo++o`     [0m[1;31m CPU:[0m 4x 0x00[0m
+[0m[1;31m               /osyyyyyyo[0m[1;33m++ooo+++/    [0m[1;31m GPU:[0m [0m
+[0m[1;31m                   ````` [0m[1;33m+oo+++o\:    [0m[1;31m RAM:[0m 1787MiB / 3908MiB[0m
+[0m[1;33m                          `oo++.      [0m
